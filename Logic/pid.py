e52b917b3e7486ef7a4e747bbc90ed4108fa2d49
John Groves <johngroves310@gmail.com>
2017-03-23 20:55:52 -0700

    Updates, fix compensation errors, etc

--- Logic/pid.py
+++ Logic/pid.py
@@ -48,8 +48,8 @@ class PID(object):
         self.set_point = set_point
         self.mode = Mode.automatic
 
-        self.set_output_limits(-40, 40)
-        self.sample_time = 1000
+        self.set_output_limits(-80, 40)
+        self.sample_time = 3000
         self.controller_direction = Direction.direct
         self.set_controller_direction(controller_direction)
         self.set_tunings(self.kp, self.ki, self.kd)
